59d117f0ed80fe0250bc4e6ce30904f1e30c8b31
TraderXylioner <treider.xylioner@gmail.com>
2024-08-02 18:55:29 +0300

    update Proxy type

--- MultiRequest/types/proxy.py
+++ MultiRequest/types/proxy.py
@@ -11,6 +11,20 @@ class Protocol(Enum):
 class Proxy(BaseModel):
     protocol: Protocol
     ip: str
-    port: int | None = None
+    port: int
     user: str | None = None
     password: str | None = None
+
+    def to_string(self):
+        if self.user:
+            return f'{self.protocol.value.lower()}://{self.user}:{self.password}@{self.ip}:{self.port}'
+        else:
+            return f'{self.protocol.value.lower()}://{self.ip}:{self.port}'
+
+    def __eq__(self, other):
+        if isinstance(other, Proxy):
+            return self.ip + str(self.port) == other.ip + str(self.port)
+        return False
+
+    def __hash__(self):
+        return hash(self.ip + str(self.port))
