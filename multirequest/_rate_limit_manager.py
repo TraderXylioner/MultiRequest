5cab1a441c5c1e097629313e5753ad186c019057
TX <treider.xylioner@gmail.com>
2025-01-21 15:14:09 +0200

    refactor time update process

--- multirequest/_rate_limit_manager.py
+++ multirequest/_rate_limit_manager.py
@@ -7,16 +7,22 @@ class RateLimitManager:
         self.services = services
         self.proxies = proxies
         self.rate_limits = {}
-        self.time_update = time.time()
 
     def create_manager(self):
         for _service in self.services:
-            self.rate_limits.setdefault(_service.name, {}).update({proxy: _service.rate_limit for proxy in self.proxies})
-        self.time_update = time.time()
+            self.rate_limits.setdefault(_service.name, {})['proxies'] = {proxy: _service.rate_limit for proxy in self.proxies}
+            self.rate_limits[_service.name]['time_update'] = time.time()
+            self.rate_limits[_service.name]['time'] = _service.time_to_update
+            self.rate_limits[_service.name]['service_obj'] = _service
+
+    def update_manager(self, service):
+        self.rate_limits[service.name]['proxies'] = {proxy: service.rate_limit for proxy in self.proxies}
+        self.rate_limits[service.name]['time_update'] = time.time()
 
     async def start(self):
         self.create_manager()
         while True:
-            if time.time() >= self.time_update + 1:
-                self.create_manager()
+            for i in self.rate_limits:
+                if time.time() >= self.rate_limits[i]['time_update'] + self.rate_limits[i]['time']:
+                    self.update_manager(self.rate_limits[i]['service_obj'])
             await asyncio.sleep(0)
