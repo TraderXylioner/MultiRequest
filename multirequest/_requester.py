c42723fae154e5b2d2226cba2d6935e0c708d0f2
TX <treider.xylioner@gmail.com>
2025-02-02 20:31:31 +0200

    move raise error

--- multirequest/_requester.py
+++ multirequest/_requester.py
@@ -15,28 +15,29 @@ class Requester:
         self.timeout = timeout
 
     async def processing_request(self, request: Request, rate_limits: dict) -> Request:
-        service = rate_limits[request.service.name]
-        while True:
-            proxy = service['proxies'][service['target']]
-            if proxy[1] > 0:
-                proxy[1] -= 1
-                service['target'] = 0 if service['target'] == len(service['proxies']) - 1 else service['target'] + 1
-                data, response_object = await self.send_request(request, proxy[0])
-                request.response = Response(data=data, object=response_object)
-                return request
+        try:
+            service = rate_limits[request.service.name]
+            while True:
+                proxy = service['proxies'][service['target']]
+                if proxy[1] > 0:
+                    proxy[1] -= 1
+                    service['target'] = 0 if service['target'] == len(service['proxies']) - 1 else service['target'] + 1
+                    data, response_object = await self.send_request(request, proxy[0])
+                    request.response = Response(data=data, object=response_object)
+                    return request
 
-            await asyncio.sleep(0)
+                await asyncio.sleep(0)
+        except Exception as ex:
+            if self.is_raise_error:
+                raise f'Request error: {ex}\n{request}'
 
     async def send_request(self, request: Request, proxy: Proxy) -> tuple[bytes, ClientResponse]:
         _local_attempts = self.attempts
         while _local_attempts:
-            try:
-                _local_attempts -= 1
-                _proxy = None if proxy == 'localhost' else proxy.to_string()
-                async with aiohttp.ClientSession(trust_env=True, headers=request.headers) as session:
-                    async with _dispatch_request(session, request.method.value)(**request.get_request_params(),
-                                                                                ssl=True, proxy=_proxy, timeout=self.timeout) as response:
-                        return await response.content.read(), response
-            except Exception as ex:
-                if self.is_raise_error:
-                    raise f'Request error: {ex}\n{request} | {proxy}'
+            _local_attempts -= 1
+            _proxy = None if proxy == 'localhost' else proxy.to_string()
+            async with aiohttp.ClientSession(trust_env=True, headers=request.headers) as session:
+                async with _dispatch_request(session, request.method.value)(**request.get_request_params(),
+                                                                            ssl=True, proxy=_proxy,
+                                                                            timeout=self.timeout) as response:
+                    return await response.content.read(), response
