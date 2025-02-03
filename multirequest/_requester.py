5e4d0c5db0e26e8efb8e571a52d80aa10f168eda
TX <treider.xylioner@gmail.com>
2025-02-03 18:47:38 +0200

    debug

--- multirequest/_requester.py
+++ multirequest/_requester.py
@@ -15,14 +15,10 @@ class Requester:
         self.timeout = timeout
 
     async def processing_request(self, request: Request, rate_limits: dict) -> Request:
-        try:
             service = rate_limits[request.service.name]
             data, response_object = await self.send_request(request, service)
             request.response = Response(data=data, object=response_object)
             return request
-        except Exception as ex:
-            if self.is_raise_error:
-                raise RuntimeError(f'Request error: {ex}\n{request}')
 
     async def get_proxy(self, service: Service) -> Proxy:
         while True:
@@ -39,8 +35,13 @@ class Requester:
             _local_attempts -= 1
             proxy = await self.get_proxy(service)
             _proxy = None if proxy == 'localhost' else proxy.to_string()
-            async with aiohttp.ClientSession(trust_env=True, headers=request.headers) as session:
-                async with _dispatch_request(session, request.method.value)(**request.get_request_params(),
-                                                                            ssl=True, proxy=_proxy,
-                                                                            timeout=self.timeout) as response:
-                    return await response.content.read(), response
+            try:
+                async with aiohttp.ClientSession(trust_env=True, headers=request.headers) as session:
+                    async with _dispatch_request(session, request.method.value)(**request.get_request_params(),
+                                                                                ssl=True, proxy=_proxy,
+                                                                                timeout=self.timeout) as response:
+                        return await response.content.read(), response
+            except Exception as ex:
+                if self.is_raise_error:
+                    raise RuntimeError(f'Request error: {ex}\n{request}')
+
